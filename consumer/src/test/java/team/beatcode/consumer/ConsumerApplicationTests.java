6ef71f17f591cfa31067b10f9d35e242186b7c63
KarH-in-dream <aubergineyoukai@163.com>
2023-06-29 14:40:05 +0800

    添加了消费者及其readme说明

---
+++ consumer/src/test/java/team/beatcode/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package team.beatcode.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
