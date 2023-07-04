a2f483201aee6f4ae312970fcfcd05bd1a6f07a3
KarH-in-dream <aubergineyoukai@163.com>
2023-07-04 09:40:18 +0800

    auth:注册

--- auth/src/main/java/team/beatcode/auth/controller/LogController.java
+++ auth/src/main/java/team/beatcode/auth/controller/LogController.java
@@ -9,8 +9,10 @@ import team.beatcode.auth.dao.IpAuthDao;
 import team.beatcode.auth.dao.UserAuthDao;
 import team.beatcode.auth.entity.IpAuth;
 import team.beatcode.auth.entity.UserAuth;
+import team.beatcode.auth.feign.UserFeign;
 import team.beatcode.auth.utils.IpInHttp;
 import team.beatcode.auth.utils.IpInStr;
+import team.beatcode.auth.utils.Macros;
 import team.beatcode.auth.utils.msg.Message;
 import team.beatcode.auth.utils.msg.MessageEnum;
 
@@ -22,18 +24,30 @@ public class LogController {
     private HttpServletRequest request;
 
     @Autowired
+    private UserFeign userFeign;
+
+    @Autowired
     private IpAuthDao ipAuthDao;
     @Autowired
     private UserAuthDao userAuthDao;
 
+    private void saveLogin(String ip, int uid) {
+        long currentTime = System.currentTimeMillis();
+        IpAuth ipAuth = new IpAuth();
+        ipAuth.setIpAddr(IpInStr.ipAddrStrToHex(ip));
+        ipAuth.setUserId(uid);
+        ipAuth.setLastLogin(currentTime);
+        ipAuth.setLastFresh(currentTime);
+        ipAuthDao.save(ipAuth);
+    }
+
     /**
-     *
      * @param map 参数：<br/>
      *            {<br/>
      *            &emsp;&emsp;"name": string, // 用户名<br/>
      *            &emsp;&emsp;"pass": string  // 密码<br/>
      *            }<br/>
-     * @return bookstore经典Message格式
+     * @return bookstore经典Message格式，不含数据
      */
     @RequestMapping("/Login")
     public Message login(@RequestBody Map<String, Object> map) {
@@ -56,13 +70,7 @@ public class LogController {
                 return new Message(MessageEnum.USER_BAD_PASS_FAIL);
 
             // 记录
-            long currentTime = System.currentTimeMillis();
-            IpAuth ipAuth = new IpAuth();
-            ipAuth.setIpAddr(IpInStr.ipAddrStrToHex(ip_str));
-            ipAuth.setUserId(auth.getId());
-            ipAuth.setLastLogin(currentTime);
-            ipAuth.setLastFresh(currentTime);
-            ipAuthDao.save(ipAuth);
+            saveLogin(ip_str, auth.getId());
 
             return new Message(MessageEnum.SUCCESS);
         } catch (NullPointerException e) {
@@ -71,6 +79,9 @@ public class LogController {
         }
     }
 
+    /**
+     * @return bookstore经典Message格式，不含数据
+     */
     @RequestMapping("/Logout")
     public Message logout() {
         String ip_str = IpInHttp.getIpAddr(request);
@@ -86,9 +97,47 @@ public class LogController {
         return new Message(MessageEnum.SUCCESS);
     }
 
+    /**
+     * @param map 参数：<br/>
+     *            {<br/>
+     *            &emsp;&emsp;"name": string, // 用户名<br/>
+     *            &emsp;&emsp;"pass": string,  // 密码<br/>
+     *            &emsp;&emsp;以及user服务记录用户信息所需的更多参数<br/>
+     *            }<br/>
+     * @return bookstore经典Message格式，不含数据
+     */
     @RequestMapping("/Register")
-    public String register() {
-        // TODO
-        return null;
+    public Message register(@RequestBody Map<String, Object> map) {
+        try {
+            String KEY_PASSWORD = "pass";
+            String name = map.get("name").toString();
+            String pass = map.get(KEY_PASSWORD).toString();
+
+            String ip_str = IpInHttp.getIpAddr(request);
+            // 无法解析的
+            if (ip_str == null)
+                return new Message(MessageEnum.IP_FAULT);
+
+            // 生成新用户
+            UserAuth auth = new UserAuth();
+            auth.setName(name);
+            auth.setPass(pass);
+            auth.setRole(Macros.AUTH_CODE_USER);
+            auth = userAuthDao.save(auth);
+
+            // 记录
+            Integer id = auth.getId();
+            saveLogin(ip_str, id);
+
+            // 调用user服务记录更多信息
+            map.remove(KEY_PASSWORD);
+            map.put("user_id", id);
+            userFeign.update(map);
+
+            return new Message(MessageEnum.SUCCESS);
+        } catch (NullPointerException e) {
+            // 缺少参数
+            return new Message(MessageEnum.PARAM_FAIL);
+        }
     }
 }
--- auth/src/main/java/team/beatcode/auth/feign/UserFeign.java
+++ auth/src/main/java/team/beatcode/auth/feign/UserFeign.java
@@ -3,8 +3,14 @@ package team.beatcode.auth.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @FeignClient(value="user",configuration =
         {FeignClientsConfiguration.class})
 public interface UserFeign {
+    @RequestMapping("update")
+    void update(@RequestBody Map<String, Object> map);
 }
