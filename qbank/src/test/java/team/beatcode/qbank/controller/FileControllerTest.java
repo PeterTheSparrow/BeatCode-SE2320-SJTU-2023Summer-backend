478daed82d33a9ce22b81c0f02cf7b1ed0c89de1
iqichen <Adam20021121@gmail.com>
2023-07-13 11:27:57 +0800

    完成qbankservice测试

---
+++ qbank/src/test/java/team/beatcode/qbank/controller/FileControllerTest.java
@@ -0,0 +1,12 @@
+package team.beatcode.qbank.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileControllerTest {
+
+    @Test
+    void getTestCase() {
+    }
+}
---
+++ qbank/src/test/java/team/beatcode/qbank/controller/ProblemControllerTest.java
@@ -0,0 +1,16 @@
+package team.beatcode.qbank.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProblemControllerTest {
+
+    @Test
+    void getProblemList() {
+    }
+
+    @Test
+    void getProblemDetail() {
+    }
+}
